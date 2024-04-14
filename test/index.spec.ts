ae78a7125929f70cd0769d69b023c5bf2f9fe7ae
soul <opensource@aerrobert.com>
2024-04-14 09:29:10 -0700

    Sun Apr 14 09:29:10 PDT 2024

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,18 +3,5 @@ import { CacheOnDisk } from '../src/implementations/cache/cache-on-disk';
 import { AnthropicChatBasedLLM } from '../src/implementations/chat-based-llm/anthropic';
 
 it('should pass', async () => {
-    const hyper = new Hyper({
-        logs: {
-            disabled: true,
-        },
-        using: {
-            chatBasedLLM: new AnthropicChatBasedLLM({
-                cache: new CacheOnDisk('.hyper/cache/anthropic'),
-            }),
-        },
-    });
-
-    const h = hyper.begin();
-    await h.ask('what is 10 * 10?');
-    console.log(h.state);
+    expect(true).toBe(true);
 });
